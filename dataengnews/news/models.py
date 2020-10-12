52b4dd4a26f39ead7a85a191158fa7a63cc4cd45
Christian Liisberg <christian.liisberg@gmail.com>
2020-10-12 10:52:44 +0200

    Create news item

--- dataengnews/news/models.py
+++ dataengnews/news/models.py
@@ -2,4 +2,9 @@ from django.db import models
 
 
 class Item(models.Model):
-    url = models.URLField(max_length=200)
+    title = models.CharField(max_length=200, blank=True, null=True)
+    url = models.URLField(max_length=200, blank=True, null=True)
+    text = models.CharField(max_length=1024, blank=True, null=True)
+
+    def __str__(self):
+        return self.text if self.text else "{} - {}".format(self.title, self.url)
