8d2e9659b57f2493873d6109d20f75e51daa0c90
Christian Liisberg <christian.liisberg@gmail.com>
2020-10-13 07:05:00 +0200

    remove create_item view as requested

--- dataengnews/news/views.py
+++ dataengnews/news/views.py
@@ -8,16 +8,3 @@ from news.models import Item
 def index(request):
     items = Item.objects.all()
     return HttpResponse("Found the following items : {}\n".format([i.__str__() for i in items]))
-
-
-@require_http_methods(["POST"])
-def create_item(request):
-    item = Item()
-    if request.POST.get("text"):
-        item.text = request.POST.get("text")
-    else:
-        item.title = request.POST.get("title")
-        item.url = request.POST.get("url")
-    item.save()
-
-    return HttpResponse("Created item {}".format(item.__str__()))
