3acf092277b96c86b254c9158f69c6604655fb10
Christian Liisberg <christian.liisberg@gmail.com>
2020-10-14 12:49:59 +0200

    Added CRUD operations

--- dataengnews/news/urls.py
+++ dataengnews/news/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:item_id>/', views.get_item, name='get_item'),
+    path('create/', views.create_item, name='create_item'),
+    path('delete/<int:item_id>/', views.delete_item, name='delete_item'),
 ]
-     
--- dataengnews/news/views.py
+++ dataengnews/news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views.decorators.http import require_http_methods
 from news.models import Item
@@ -8,3 +8,28 @@ from news.models import Item
 def index(request):
     items = Item.objects.all()
     return HttpResponse("Found the following items : {}\n".format([i.__str__() for i in items]))
+
+
+@require_http_methods(["GET"])
+def get_item(request, item_id):
+    item = get_object_or_404(Item, pk=item_id)
+    return HttpResponse("Found item {}".format(item))
+
+
+@require_http_methods(["POST"])
+def create_item(request):
+    item = Item()
+    if request.POST.get("text"):
+        item.text = request.GET.get("text")
+    else:
+        item.title = request.POST.get("title")
+        item.url = request.POST.get("url")
+    item.save()
+    return HttpResponse("Created item {}".format(item))
+
+
+@require_http_methods(["POST"])
+def delete_item(request, item_id):
+    item = get_object_or_404(Item, pk=item_id)
+    item.delete()
+    return HttpResponse("Deleted item id {}".format(item_id))
