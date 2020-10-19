3f0cfd567b3a852e3b32ed8923e5a4d6de3d6688
Christian Liisberg <christian.liisberg@gmail.com>
2020-10-19 09:32:21 +0200

    create news item and get specific item

--- dataengnews/news/urls.py
+++ dataengnews/news/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:item_id>/', views.get_item, name='get_item'),
+    path('create/', views.create_item, name='create_item'),
 ]
      
